6a136825cfee4c2fbed73ad04018ec9376f62b40
Raphael Lopes <ph.campanario07@gmail.com>
2021-05-22 16:33:41 -0300

    add prop color from Button component

--- src/components/shared/buttons/default.jsx
+++ src/components/shared/buttons/default.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import classNames from 'classnames';
 
-export const Button = ({ className, type = 'button', full = true, ...rest }) => {
-  const styleClass = 'group relative flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-purple-600 hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500';
+export const Button = ({
+  className,
+  type = 'button',
+  full = true,
+  color = 'purple',
+  ...rest
+}) => {
+  const styleClass = `group relative justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-${color}-600 hover:bg-${color}-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-${color}-500`;
   return (
     <button
       className={classNames(styleClass, full && 'w-full', className)}
