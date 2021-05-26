42ff2895b879c23fe087357d09fd63875e73fdda
Raphael Lopes <ph.campanario07@gmail.com>
2021-05-26 15:50:14 -0300

    Fix email verification layout and improve Readme file

--- README.md
+++ README.md
@@ -1,34 +1,49 @@
 # The Road to React with Firebase
 
 This project was written while I was studying the Firebase fundamentals reading
-the book The Road to React with Firebase.
+the book [The Road to React with Firebase](https://www.robinwieruch.de/the-road-to-react-with-firebase-book).
 
 ## Topics of studying
 - Autentication
 - Autentication with Social Logins
 - Link accounts with different Social Logins
-- HOC to control access pages from autenticated users and admin roles
+- Protected Routes with Authorization using HOC
+- Roles-based Authorization
+- Database with Users and Messages
 - Deploy with firebase
+- For the layout I used [tailwindcss](https://tailwindcss.com/).
+
+## Demo
+- To see the final result, you can open this [link](https://book-react-with-firebase.web.app/)
+
+> The landing page has no features and this route is public. I will add some
+  feature in the future and manage its content with admin route.
+
+  You can sign up an user with admin role using the sign up route. It is not a
+  good practice to keep this flag, but this project is only for studying, so 
+  I keep it there
 
 ## Available Scripts
 
 In the project directory, you can run:
+
 
 ### `yarn start`
 
 Runs the app in the development mode.\
 Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
 
-The page will reload if you make edits.\
-You will also see any lint errors in the console.
 
 ### `yarn build`
 
 Builds the app for production to the `build` folder.\
 It correctly bundles React in production mode and optimizes the build for the best performance.
 
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+### `firebase`
 
+You try locally you must create a Firebase project, and set the `.env` file based
+on `.env.example` file with the environment variables.
+
+After that, you must login locally with you firebase account. You can follow
+the [official docs](https://firebase.google.com/docs/web/setup).
--- src/components/Session/withEmailVerification.jsx
+++ src/components/Session/withEmailVerification.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useState } from 'react';
 
 import { withFirebase } from '../Firebase';
 import AuthUserContext from './context';
+import { Container, HeaderContainer, Content, Button } from '../shared';
 
 const needsEmailVerification = authUser =>
   authUser &&
@@ -19,25 +20,34 @@ const withEmailVerification = Component => {
     }
 
     // renders
-    const renderMessage = isSent ? (
-      <p className="text-center font-extrabold text-gray-900">
-        E-Mail confirmation sent: Check your E-Mails (spam folder inclided) for a
-        confirmation E-Mail. Refresh this page once you confirmed your E-Mail.
-      </p>
-    ) : (
-      <div>
-        <p className="text-center font-extrabold text-gray-900">
-          Verify your E-Mail: Check your E-Mails (spam folder included) for a
-          confirmation E-Mail or send another confirmation E-Mail.
-        </p>
-        <button
-          type="button"
-          onClick={handleSendEmailVerification}
-          disabled={isSent}
-          className={`${isSent ? 'disabled:opacity-50' : ''} group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-purple-600 hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500`}>
-          Send confirmation
-        </button>
-      </div>
+    const renderMessage = (
+      <>
+        <HeaderContainer>
+          Confirm your E-Mail
+        </HeaderContainer>
+        <Container>
+          <Content>
+            {isSent ? (
+              <p className="font-extrabold text-center text-gray-900">
+                E-Mail confirmation sent: Check your E-Mails (spam folder inclided) for a
+                confirmation E-Mail. Refresh this page once you confirmed your E-Mail.
+              </p>
+            ) : (
+              <div>
+                <p className="mb-6 font-extrabold text-center text-gray-900">
+                  Verify your E-Mail: Check your E-Mails (spam folder included) for a
+                  confirmation E-Mail or send another confirmation E-Mail.
+                </p>
+                <Button
+                  onClick={handleSendEmailVerification}
+                >
+                  Send confirmation
+                </Button>
+              </div>
+            )}
+          </Content>
+        </Container>
+      </>
     );
 
     return needsEmailVerification(authUser) ? renderMessage : <Component {...props} />
